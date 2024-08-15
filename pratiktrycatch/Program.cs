27b1373cecd7e2d017a8551e1d77a7db47c79d3d
ZEK <ZEK@DESKTOP-MQI8H1N>
2024-08-15 11:35:04 +0300

    Proje dosyası ekle.

---
+++ pratiktrycatch/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Lütfen bir sayı giriniz : ");
+
+try
+{
+    // Kullanıcının girdiği değeri al ve integer'a çevir
+    int sayi = int.Parse(Console.ReadLine());
+    // Sayının karesini hesapla ve ekrana yazdır
+    int kare = sayi * sayi;
+    Console.WriteLine("gridiğiniz sayının karesi: " + kare);
+}
+catch (FormatException)
+{
+    // Kullanıcı geçersiz bir giriş yaptığında hata mesajını göster
+    Console.WriteLine("geçersiz giriş ");
+}
